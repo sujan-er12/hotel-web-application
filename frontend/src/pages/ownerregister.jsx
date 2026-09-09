56085be7383740b3bf3226fe26ac37382e71d0f7
sujaner <sujanergowda12@.com>
2026-09-09 13:30:05 +0530

    Fix CSS filename case

--- frontend/src/pages/ownerregister.jsx
+++ frontend/src/pages/ownerregister.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import "./OwnerRegister.css";
+import "./ownerregister.css";
 
 function OwnerRegister() {
     const navigate = useNavigate();
