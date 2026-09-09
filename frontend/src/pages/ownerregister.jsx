953271b484128647651336fbf86727578e281027
sujaner <sujanergowda12@.com>
2026-09-09 17:25:26 +0530

    Fix the api to connect backend and frontend

--- frontend/src/pages/ownerregister.jsx
+++ frontend/src/pages/ownerregister.jsx
@@ -29,7 +29,7 @@ function OwnerRegister() {
 
         try {
             const response = await fetch(
-                "http://127.0.0.1:5000/api/owner/register",
+                "https://hotel-web-application-bmc5.onrender.com/api/owner/register",
                 {
                     method: "POST",
                     headers: {
--- frontend/src/pages/userlogin.jsx
+++ frontend/src/pages/userlogin.jsx
@@ -18,7 +18,7 @@ function UserLogin() {
 
         try {
             const response = await fetch(
-                "http://127.0.0.1:5000/api/user/login",
+                "https://hotel-web-application-bmc5.onrender.com/api/user/login",
                 {
                     method: "POST",
                     headers: {
